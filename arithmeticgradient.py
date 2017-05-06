927f31aad40936f55b1408bb0e3b200d5c615692
Jay <mentoce@gmail.com>
2017-05-06 12:23:55 -0700

    removed source from root folder

--- arithmeticgradient.py
+++
@@ -1,5 +0,0 @@
-def uniform_series(g, i, n):
-    return g*(1/i - n/((1 + i)**n - 1))
-
-def present_worth(g, i, n):
-    return g*(1/i**2 - (i*n - 1)/((i**2)*((1 + i)**n)))
--- singlepayment.py
+++
@@ -1,5 +0,0 @@
-def compound_amount(p, i, n):
-    return p*(1 + i)**n
-
-def present_worth(f, i, n):
-    return f*(1 + i)**(-n)
